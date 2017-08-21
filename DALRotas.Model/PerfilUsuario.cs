e1e49e1354812c96b1d6fece0e9687abf387ba22
Thiago D. Pereira <Thiago D. Pereira@DESKTOP-2NFLIAK>
2017-08-21 13:07:19 -0400

    Estrutura Usuarios

---
+++ DALRotas.Model/PerfilUsuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALRotas.Model
+{
+    public class PerfilUsuario
+    {
+        public int IdPerfilUsuario { get; set; }
+        public string Descricao { get; set; }
+    }
+}
---
+++ DALRotas.Model/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALRotas.Model
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public DateTime DtNascimento { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        public int IdPerfilUsuario { get; set; }
+
+    }
+}
